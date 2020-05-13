28177f01445288b56df0f315140ed15079fbb159
Amelie <a.grn33@gmail.com>
2020-05-13 14:12:10 +0200

    Q05 route show added

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,17 +6,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+/**
+ * Class WildController
+ * @package App\Controller
+ * @Route("/wild", name="wild_")
+ */
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
-            'website' => 'Wild Séries',
+            'website' => 'wild Séries',
         ]);
     }
+
+    /**
+     * @Route("/show/{slug}",requirements={"slug" = "[a-z0-9-]+"}, name="show")
+     * @param string|null $slug
+     * @return Response
+     */
+    public function show(?string $slug): Response
+    {
+        if ($slug){
+            $slug = ucwords(str_replace('-', ' ', $slug));
+        }else {
+            $slug = 'Aucune série sélectionnée, veuillez choisir une série';
+        }
+            return $this->render('wild/show.html.twig', ['slug' => $slug]);
+        }
+
 }
 
