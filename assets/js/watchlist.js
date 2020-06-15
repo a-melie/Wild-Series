594427166522ecaeb5c6bf78350c3798374c7803
Amelie <a.grn33@gmail.com>
2020-06-15 11:48:21 +0200

    ajax done with classname on index and shpw

--- assets/js/watchlist.js
+++ assets/js/watchlist.js
@@ -1,17 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const watchlist = document.getElementById('watchlist');
-    watchlist.addEventListener("click", (event)=> {
-        event.preventDefault();
-        watchlist.classList.toggle('active');
-        const link = watchlist.dataset.href;
-        fetch(link)
-            .then(function (res) {
-                return res.text()
-            })
-            .then(function (json) {
-                const response = JSON.parse(json);
-            })
-    })
+    const watchlist = document.getElementsByClassName('watchlist');
+    for ( let i = 0; i<watchlist.length; i++){
+        watchlist[i].addEventListener("click", (event)=> {
+            event.preventDefault();
+            watchlist[i].classList.toggle('active');
+            const link = watchlist[i].dataset.href;
+            fetch(link)
+                .then(function (res) {
+                    return res.text()
+                })
+                .then(function (json) {
+                    const response = JSON.parse(json);
+                })
+        })
+    }
+
 })
