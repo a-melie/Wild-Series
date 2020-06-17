d7342bfae5b3881535f684ffa54ab58edf935af2
Amelie <a.grn33@gmail.com>
2020-06-17 19:22:52 +0200

    video adde in readme for quest26

--- src/Controller/NavBarController.php
+++ src/Controller/NavBarController.php
@@ -6,10 +6,15 @@ namespace App\Controller;
 
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class NavBarController extends AbstractController
 {
-    public function selectCategories(CategoryRepository $categoryRepository)
+    /**
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
+    public function selectCategories(CategoryRepository $categoryRepository): Response
     {
         $categories = $categoryRepository->findAll();
         return $this->render('components/_selectCategories.html.twig', [
